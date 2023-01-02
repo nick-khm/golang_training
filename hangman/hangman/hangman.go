baf6316483f03ec720883c0fd68c94dec5f13074
Nick Kharaim <nick.kharaim@gmail.com>
2023-01-02 20:42:52 +0100

    Tests 1

--- hangman/hangman/hangman.go
+++ hangman/hangman/hangman.go
@@ -1,6 +1,9 @@
 package hangman
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 type GameStateType string
 
@@ -20,7 +23,11 @@ type Game struct {
 	TurnsLeft    int           // Remaining attempts
 }
 
-func New(turns int, word string) *Game {
+func New(turns int, word string) (*Game, error) {
+	if len(word) < 3 {
+		return nil, fmt.Errorf("word '%s' must be at least 3 chars, got=%v", word, len(word))
+	}
+
 	letters := strings.Split(strings.ToUpper(word), "")
 	found := make([]string, len(letters))
 	for i := 0; i < len(letters); i++ {
@@ -34,7 +41,7 @@ func New(turns int, word string) *Game {
 		UsedLetters:  []string{},
 		TurnsLeft:    turns,
 	}
-	return g
+	return g, nil
 }
 
 func (g *Game) MakeAGuess(guess string) {
---
+++ hangman/hangman/hangman_test.go
@@ -0,0 +1,45 @@
+package hangman
+
+import "testing"
+
+func TestLetterInWord(t *testing.T) {
+	word := []string{"b", "o", "b"}
+	guess := "b"
+	hasLetter := letterInWord(guess, word)
+	if !hasLetter {
+		t.Errorf("Word %s contains letter %s. got=%v", word, guess, hasLetter)
+	}
+
+}
+
+func TestLetterNotInWord(t *testing.T) {
+	word := []string{"b", "o", "b"}
+	guess := "a"
+	hasLetter := letterInWord(guess, word)
+	if hasLetter {
+		t.Errorf("Word %s doesn't contain letter %s. got=%v", word, guess, hasLetter)
+	}
+
+}
+
+func TestInvalidWord(t *testing.T) {
+	_, err := New(3, "")
+	if err == nil {
+		t.Errorf("Error should be returned when using invalid word=''")
+	}
+
+}
+
+func TestGameGoodGuess(t *testing.T) {
+	g, _ := New(3, "bob")
+	g.MakeAGuess("b")
+	validState(t, GoodGuess, g.State)
+}
+
+func validState(t *testing.T, expectedState, actualState GameStateType) bool {
+	if expectedState != actualState {
+		t.Errorf("state should be '%v', got=%v", expectedState, actualState)
+		return false
+	}
+	return true
+}
