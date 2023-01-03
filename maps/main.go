97bd79eb0f331340daa0326de8f0f347427ec8b4
Nick Kharaim <nick.kharaim@gmail.com>
2023-01-03 19:15:00 +0100

    Loop on maps

--- maps/main.go
+++ maps/main.go
@@ -3,35 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	m := make(map[string]int)
-	fmt.Printf("Map content %v (len=%v)\n", m, len(m))
+	m := map[string]int{
+		"Bob":     11,
+		"Alice":   22,
+		"Bobette": 33,
+	}
+	fmt.Printf("m=%v\n", m)
 
-	m["hello"] = 5
-	m["goodbye"] = 10
-	fmt.Printf("Map content %v (len=%v)\n", m, len(m))
-
-	fmt.Printf("key=value, value=%v\n", m["hello"])
-
-	i := m["goodbye"]
-	fmt.Println(i)
-
-	// error on key => init à default value and test key presence
-	j, ok := m["hel"]
-	fmt.Printf("j=%v and existance of key OK=%v\n", j, ok)
-
-	// test online
-	m["beatles"] = 2
-	if _, testOk := m["beatles"]; testOk {
-		fmt.Println("Beatles is OK")
+	for name, age := range m {
+		fmt.Printf("name=%s age=%v\n", name, age)
 	}
 
-	delete(m, "beatles")
-	fmt.Printf("Map content %v (len=%v)\n", m, len(m))
-
-	m2 := m
-	fmt.Printf("m2 content %v (len=%v)\n", m2, len(m2))
-	m2["help"] = 44
-	fmt.Printf("Map content %v (len=%v)\n", m, len(m))
-	fmt.Printf("m2 content %v (len=%v)\n", m2, len(m2))
+	i := 1
+	for name := range m {
+		m[name] = i
+		i++
+	}
+	fmt.Printf("m=%v\n", m)
 
 }
