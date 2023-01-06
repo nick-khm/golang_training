325ac8b51fd8f0931ccdd7299cea4bf0f92bd411
Nick Kharaim <nick.kharaim@gmail.com>
2023-01-06 10:03:32 +0100

    Dictionary with CLI actions

--- dictionary/main.go
+++ dictionary/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,15 +9,54 @@ import (
 )
 
 func main() {
+	action := flag.String("action", "list", "Action to performe on the dictionary")
+
 	d, err := dictionary.New("./badger")
 	handleError(err)
 	defer d.Close()
 
-	d.Remove("toto")
+	flag.Parse()
+	switch *action {
+	case "list":
+		actionList(d)
+	case "add":
+		actionAdd(d, flag.Args())
+	case "define":
+		actionGet(d, flag.Args())
+	case "remove":
+		actionRemove(d, flag.Args())
+	default:
+		fmt.Printf("Unknown action %v\n", *action)
+	}
+}
+
+func actionRemove(d *dictionary.Dictionary, args []string) {
+	word := args[0]
+	err := d.Remove(word)
+	handleError(err)
+	fmt.Printf("'%v' was removed\n", word)
+}
+
+func actionGet(d *dictionary.Dictionary, args []string) {
+	word := args[0]
+	entry, err := d.Get(word)
+	handleError(err)
+	fmt.Printf("'%v' - '%v'\n", entry.Word, entry.Definition)
+}
+
+func actionAdd(d *dictionary.Dictionary, args []string) {
+	word := args[0]
+	definition := args[1]
+	err := d.Add(word, definition)
+	handleError(err)
+	fmt.Printf("'%v' added to the dictionary\n", word)
+}
+
+func actionList(d *dictionary.Dictionary) {
 	words, entries, err := d.List()
 	handleError(err)
+	fmt.Println("Dictionary content")
 	for _, word := range words {
-
 		fmt.Println(entries[word])
 	}
 }
