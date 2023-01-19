a220b199228f6bce95e359d19ed5a5f64511faf5
Nick Kharaim <nick.kharaim@gmail.com>
2023-01-19 21:15:22 +0100

    Generate pdf or html via CLI

--- certificate/main.go
+++ certificate/main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"os"
 
 	"training.go/certificate/cert"
 	"training.go/certificate/cert/html"
+	"training.go/certificate/pdf"
 )
 
 func main() {
-	c, err := cert.New("Golang programming", "Bob Dylan", "2023-01-11")
+	outputType := flag.String("type", "pdf", "Output type of the certificate.")
+	flag.Parse()
+
+	var saver cert.Saver
+	var err error
+	switch *outputType {
+	case "html":
+		saver, err = html.New("output")
+	case "pdf":
+		saver, err = pdf.New("output")
+	default:
+		fmt.Printf("Unkown output type. got=%v\n", *outputType)
+		os.Exit(1)
+	}
 	if err != nil {
 		fmt.Printf("Error during certificate creation: %v", err)
 		os.Exit(1)
 	}
 
-	var saver cert.Saver
-	saver, err = html.New("output")
+	c, err := cert.New("Golang programming", "Bob Dylan", "2023-01-11")
 	if err != nil {
-		fmt.Printf("Error during pdf creation: %v", err)
+		fmt.Printf("Error during certificate creation: %v", err)
 		os.Exit(1)
 	}
 	saver.Save(*c)
