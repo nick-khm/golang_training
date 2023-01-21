23112913122996cce9771e2d138e8c3782fab54d
Nick Kharaim <nick.kharaim@gmail.com>
2023-01-21 09:19:53 +0100

    Unidirectional channels

--- channels/main.go
+++ channels/main.go
@@ -5,43 +5,17 @@ import (
 	"time"
 )
 
-func ping(c chan string) {
-	for i := 1; ; i++ {
-		c <- fmt.Sprintf("ping %v", i)
-	}
-}
-
-func pong(c chan string) {
-	for i := 100; ; i += 100 {
-		c <- fmt.Sprintf("pong %v", i)
-	}
-}
-
-func print(c chan string) {
-	for {
-		msg := <-c
-		fmt.Println(msg)
-		time.Sleep(1 * time.Second)
-	}
-}
-
-func one() {
-	fmt.Println("one")
-}
-
-func hello(c chan string) {
-	c <- "Hello there" // blocked until red by someone else
-	fmt.Println("hello: finised")
-	fmt.Printf("hello: %v\n", <-c)
+func longOperation(done chan<- bool) {
+	fmt.Println("longOperation: Started long operation...")
+	time.Sleep(2 * time.Second)
+	fmt.Println("longOperation: Done!")
+	done <- true
 }
 
 func main() {
-	c := make(chan string)
+	done := make(chan bool)
+	go longOperation(done)
+	<-done
+	fmt.Println("Back to main")
 
-	go hello(c)
-	msg := <-c
-	fmt.Printf("main %v\n", msg)
-	c <- "Main are writing"
-
-	time.Sleep(10 * time.Second)
 }
